da0a66f70a75a3fe02d7544293393ab491604b07
dra1n <dra1n86@gmail.com>
2015-03-07 07:48:41 +0200

    Get rid of jQuery.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,5 @@
 require("!style!css!less!../stylesheets/main.less");
 
-var $ = require("jquery");
 var React = require("react");
 
 var products = require('./data/products');
@@ -8,31 +7,7 @@ var AppComponent = require('./components/AppComponent');
 
 window.React = React;
 
-function renderApp() {
-  React.render(
-    <AppComponent products={products}/>,
-    document.getElementById('app')
-  );
-}
-
-
-$(function() {
-  renderApp();
-
-  /* Cart handlers */
-
-  //$('#app').on('click', '.delete', function(e) {
-    //e.preventDefault();
-    //var id = $(e.target).data('product');
-    //CartStore.remove(id);
-  //});
-
-  //$('#app').on('change', 'input', function(e) {
-    //var id = $(e.target).data('product');
-    //var value = $(e.target).val();
-    //var product = CartStore.get(id);
-
-    //product.quantity = parseInt(value);
-    //CartStore.update(id, product);
-  //});
-});
+React.render(
+  <AppComponent products={products}/>,
+  document.getElementById('app')
+);
